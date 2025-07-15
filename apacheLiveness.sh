12a416849677ac67d5045d840e415097cbfee60c
Orest Mahdziak <44407006+OrestIT@users.noreply.github.com>
2025-07-15 17:13:09 +0300

    Update apacheLiveness.sh

--- apacheLiveness.sh
+++ apacheLiveness.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "start checking apache status"
 
-if systemctl list-unit-files | grep -q "^httpd.service"; then
+if service apache2 list-unit-files | grep -q "^httpd.service"; then
   SERVICE_NAME="httpd"
-elif systemctl list-unit-files | grep -q "^apache2.service"; then
+elif service apache2 list-unit-files | grep -q "^apache2.service"; then
   SERVICE_NAME="apache2"
 else
   echo "Apache юніт не існує у systemd"
